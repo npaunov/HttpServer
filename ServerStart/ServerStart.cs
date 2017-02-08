3acfcce5cefbe0811d8222cbb76dbdedf1f17c29
npaunov <nvpaunov@gmail.com>
2017-02-08 23:20:06 +0200

    Added/ Deled files!

---
+++ ServerStart/ServerStart.cs
@@ -0,0 +1,34 @@
+﻿using HttpServer.Enums;
+using HttpServer.Models;
+using System.Collections.Generic;
+
+namespace ServerStart
+{
+    public class ServerStart
+    {
+        static void Main()
+        {
+            var routes = new List<Route>()
+            {
+                new Route
+                {
+                    Name = "Hello Handler",
+                    UrlRegex = @"^/hello$",
+                    Method = RequestMethod.GET,
+                    Callable = (HttpRequest request) =>
+                    {
+                        return new HttpResponse()
+                        {
+                            ContentAsUTF8 = "<h3>Hello From HttpServer! :)<h3><h2>I GOT A FLYING MACHINE<h2>",
+                            StatusCode = ResponseStatusCode.Ok
+                            
+                        };
+                    }
+                }
+            };
+
+            HttpServer.HttpServer httpServer = new HttpServer.HttpServer(8081, routes);
+            httpServer.Listen(); 
+        }
+    }
+}
